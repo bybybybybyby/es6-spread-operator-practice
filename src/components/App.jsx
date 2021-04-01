f586f5bd653a70fe717c86b1046330b9badd62cd
Brian <bybyby@gmail.com>
2021-03-31 23:08:42 -0700

    Add key value to list items.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ function App() {
       </div>
       <div>
         <ul>
-          {list.map(item => <li>{item}</li>)}
+          {list.map((item) => <li key={list.indexOf(item)}>{item}</li>)}
         </ul>
       </div>
     </div>
